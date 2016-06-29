8e7389a33a9807710f1463f5c74ab9a008331f24
David Tolnay <dtolnay@gmail.com>
2016-06-29 09:18:35 -0700

    Change serde test to use char instead of String

--- tests/serde.rs
+++ tests/serde.rs
@@ -8,7 +8,7 @@ use serde_test::{Token, assert_tokens};
 
 #[test]
 fn test_ser_de_empty() {
-    let map = LinkedHashMap::<String, u32>::new();
+    let map = LinkedHashMap::<char, u32>::new();
 
     assert_tokens(&map, &[
         Token::MapStart(Some(0)),
@@ -19,22 +19,22 @@ fn test_ser_de_empty() {
 #[test]
 fn test_ser_de() {
     let mut map = LinkedHashMap::new();
-    map.insert("b".to_string(), 20);
-    map.insert("a".to_string(), 10);
-    map.insert("c".to_string(), 30);
+    map.insert('b', 20);
+    map.insert('a', 10);
+    map.insert('c', 30);
 
     assert_tokens(&map, &[
         Token::MapStart(Some(3)),
             Token::MapSep,
-            Token::Str("b"),
+            Token::Char('b'),
             Token::I32(20),
 
             Token::MapSep,
-            Token::Str("a"),
+            Token::Char('a'),
             Token::I32(10),
 
             Token::MapSep,
-            Token::Str("c"),
+            Token::Char('c'),
             Token::I32(30),
         Token::MapEnd,
     ]);
